3edaf831928832b817d9fea263f1b4de1c3459cf
Frank McAuley <frank.mcauley@gmail.com>
2015-11-08 07:25:15 -0500

    Adding the logic for the view controller so that a tweet can be sent out.

--- SampleTweetAppl/ViewController.swift
+++ SampleTweetAppl/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func sendTweet(sender: AnyObject) {
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter){
+            presentViewController(self.sendATweet(), animated: true, completion: nil)
+        } else {
+            print(self.failedToSendATweet())
+        }
+    }
+    
+    
+    func sendATweet() -> SLComposeViewController{
+        let tweetVC = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        tweetVC.setInitialText("This is a sample tweet from a test app")
+        return tweetVC
+    }
+    
+    func failedToSendATweet() -> String {
+        return "It is not possible to send a tweet at this time"
+    }
+    
 }
 
